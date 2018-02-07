9c5a94418feb3b04a56be24e32cac4f25b386707
CarlosFloresNK <carlos.flores119@outlook.com>
2018-02-07 11:56:03 -0600

    Creation Initial Project

---
+++ ForeingEchange2/ForeingEchange2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ForeingEchange2
+{
+    public partial class ForeingEchange2Page : ContentPage
+    {
+        public ForeingEchange2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
